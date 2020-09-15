2017350c8c57041de82875ed4457d54dae6d5d63
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-09-15 08:18:13 -0500

    Quarkus REST new project scaffold

--- rest-json-quickstart/src/test/java/org/acme/rest/json/NativeFruitResourceIT.java
+++ rest-json-quickstart/src/test/java/org/acme/rest/json/NativeFruitResourceIT.java
@@ -1,9 +1,9 @@
-package org.acme.getting.started;
+package org.acme.rest.json;
 
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeFruitResourceIT extends FruitResourceTest {
 
     // Execute the same tests but in native mode.
 }
