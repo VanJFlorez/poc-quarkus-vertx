2e5ecea1d87948acef9cec3ef4ce4447fa38cdf4
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-09-15 15:45:25 -0500

    get tics resource stream

--- getting-started-reactive/src/main/java/org/acme/quickstart/ReactiveGreetingResource.java
+++ getting-started-reactive/src/main/java/org/acme/quickstart/ReactiveGreetingResource.java
@@ -1,12 +1,36 @@
 package org.acme.quickstart;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.annotations.SseElementType;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import io.smallrye.mutiny.Multi;
+
 @Path("/hello")
 public class ReactiveGreetingResource {
+
+    @Inject
+    ReactiveGreetingService service;
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/greeting/{count}/{name}")
+    public Multi<String> greetings0(@PathParam int count, @PathParam String name) {
+      return service.greetings(count, name);
+    }
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType(MediaType.TEXT_PLAIN)
+    @Path("/stream/{count}/{name}")
+    public Multi<String> greetingsAsStream(@PathParam int count, @PathParam String name) {
+        return service.greetings(count, name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
